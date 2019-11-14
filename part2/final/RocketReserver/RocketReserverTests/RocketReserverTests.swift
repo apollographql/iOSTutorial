173589cdb727466958a9950e3451410c20cf265c
Ellen Shapiro <designatednerd@gmail.com>
2019-11-14 13:53:29 -0600

    copy final of pt 1 to final of pt 2

---
+++ part2/final/RocketReserver/RocketReserverTests/RocketReserverTests.swift
@@ -0,0 +1,34 @@
+//
+//  RocketReserverTests.swift
+//  RocketReserverTests
+//
+//  Created by Ellen Shapiro on 11/13/19.
+//  Copyright © 2019 Apollo GraphQL. All rights reserved.
+//
+
+import XCTest
+@testable import RocketReserver
+
+class RocketReserverTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
