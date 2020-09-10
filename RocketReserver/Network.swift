c10824d68c5f89499944a4c9a49114ec5ae6cdd7
Ellen Shapiro <designatednerd@gmail.com>
2020-09-10 12:48:09 -0500

    Update code for updated prose

--- RocketReserver/Network.swift
+++ RocketReserver/Network.swift
@@ -17,52 +17,10 @@ class Network {
         let client = URLSessionClient()
         let cache = InMemoryNormalizedCache()
         let store = ApolloStore(cache: cache)
-        let provider = NetworkInterceptorProvider(store: store, client: client)
-        let requestChainTransport = RequestChainNetworkTransport(interceptorProvider: provider,
-                                                                 endpointURL: URL(string: "https://apollo-fullstack-tutorial.herokuapp.com/")!)
-        return ApolloClient(networkTransport: requestChainTransport)
+        let provider = NetworkInterceptorProvider(client: client, store: store)
+        let url = URL(string: "https://apollo-fullstack-tutorial.herokuapp.com/")!
+        let transport = RequestChainNetworkTransport(interceptorProvider: provider,
+                                                     endpointURL: url)
+        return ApolloClient(networkTransport: transport)
     }()
-}
-
-struct NetworkInterceptorProvider: InterceptorProvider {
-    
-    private let store: ApolloStore
-    private let client: URLSessionClient
-    
-    init(store: ApolloStore,
-         client: URLSessionClient) {
-        self.store = store
-        self.client = client
-    }
-    
-    func interceptors<Operation: GraphQLOperation>(for operation: Operation) -> [ApolloInterceptor] {
-        return [
-            MaxRetryInterceptor(),
-            LegacyCacheReadInterceptor(store: self.store),
-            TokenAddingInterceptor(),
-            NetworkFetchInterceptor(client: self.client),
-            ResponseCodeInterceptor(),
-            LegacyParsingInterceptor(cacheKeyForObject: self.store.cacheKeyForObject),
-            AutomaticPersistedQueryInterceptor(),
-            LegacyCacheWriteInterceptor(store: self.store)
-        ]
-    }
-}
-
-class TokenAddingInterceptor: ApolloInterceptor {
-    func interceptAsync<Operation: GraphQLOperation>(
-        chain: RequestChain,
-        request: HTTPRequest<Operation>,
-        response: HTTPResponse<Operation>?,
-        completion: @escaping (Result<GraphQLResult<Operation.Data>, Error>) -> Void) {
-        
-        let keychain = KeychainSwift()
-        if let token = keychain.get(LoginViewController.loginKeychainKey) {
-            request.addHeader(name: "Authorization", value: token)
-        } // else do nothing
-        
-        chain.proceedAsync(request: request,
-                           response: response,
-                           completion: completion)
-    }
 }
---
+++ RocketReserver/NetworkInterceptorProvider.swift
@@ -0,0 +1,19 @@
+//
+//  NetworkInterceptorProvider.swift
+//  RocketReserver
+//
+//  Created by Ellen Shapiro on 9/9/20.
+//  Copyright © 2020 Apollo GraphQL. All rights reserved.
+//
+
+import Foundation
+import Apollo
+
+class NetworkInterceptorProvider: LegacyInterceptorProvider {
+    
+    override func interceptors<Operation: GraphQLOperation>(for operation: Operation) -> [ApolloInterceptor] {
+        var interceptors = super.interceptors(for: operation)
+        interceptors.insert(TokenAddingInterceptor(), at: 0)
+        return interceptors
+    }
+}
