096345f51c7f0f5eeb5e03dc7278909566056149
Calvin Cestari <calvin.cestari@gmail.com>
2022-03-18 21:03:50 -0700

    Remove UI to update profile picture

--- final/RocketReserver/ProfileViewController.swift
+++ final/RocketReserver/ProfileViewController.swift
@@ -14,29 +14,22 @@ class ProfileViewController: UIViewController {
     @IBOutlet private var profileImageView: UIImageView!
     @IBOutlet private var emailLabel: UILabel!
     @IBOutlet private var idLabel: UILabel!
-    @IBOutlet private var selectImageButton: UIButton!
     
     private func configureForImageURL(urlString: String?) {
         let personImage = UIImage(systemName: "person")
         guard
             let urlString = urlString,
             let url = URL(string: urlString) else {
-                self.selectImageButton.setTitle("Select Image", for: .normal)
                 self.profileImageView.image = personImage
                 return
         }
         
-        self.selectImageButton.setTitle("Update Image", for: .normal)
         self.profileImageView.sd_setImage(with: url, placeholderImage: personImage)
     }
     
     
     @IBAction private func closeTapped() {
         self.dismiss(animated: true)
-    }
-    
-    @IBAction private func updateImageTapped() {
-        // TODO: Update the image
     }
 }
 
