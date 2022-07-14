bc76d7585664719a901c84932b567765c36e629a
Mahmudul Hasan <mhasan341@gmail.com>
2022-07-14 17:37:08 +0600

    Some Important Updates (#5)
    * Replaced unnecessary variable declaration
    * Made the conditional statement one-liner
    * More accurate email validation using regex

--- starter/RocketReserver/SceneDelegate.swift
+++ starter/RocketReserver/SceneDelegate.swift
@@ -30,11 +30,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate, UISplitViewControllerDe
     func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController:UIViewController, onto primaryViewController:UIViewController) -> Bool {
         guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
         guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
-        if topAsDetailController.launchID == nil {
-            // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
-            return true
-        }
-        return false
+        // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
+        return topAsDetailController.launchID == nil ? true : false
+        
     }
 
 }
